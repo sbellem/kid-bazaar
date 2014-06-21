3b0a1104501c0211a6dffa10dcaf299519908c4a
virtuallight <mat.jankowski@gmail.com>
2014-06-21 15:50:23 +0200

    creating a merchant placeholder

--- kid_bazaar/apps/payments/urls.py
+++ kid_bazaar/apps/payments/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from .views import pay 
+from .views import pay, create
 
 
 urlpatterns = patterns('',
     url(r'^pay/', pay),
+    url(r'^create/', create),
 )
--- kid_bazaar/apps/payments/views.py
+++ kid_bazaar/apps/payments/views.py
@@ -1,15 +1,58 @@
+# -*- coding: utf-8 -*-
 import braintree
 from django.conf import settings
 from django.shortcuts import render
 
 
-def test_pay():
-    braintree.Configuration.configure(
-        braintree.Environment.Sandbox,
-        settings.BRAINTREE_MERCHANT_ID,
-        settings.BRAINTREE_PUBLIC_KEY,
-        settings.BRAINTREE_PRIVATE_KEY
-    )
+braintree.Configuration.configure(
+    braintree.Environment.Sandbox,
+    settings.BRAINTREE_MERCHANT_ID,
+    settings.BRAINTREE_PUBLIC_KEY,
+    settings.BRAINTREE_PRIVATE_KEY
+)
+
+
+def _test_merchantaccount_create():
+    return braintree.MerchantAccount.create({
+        'individual': {
+            'first_name': "Jane",
+            'last_name': "Doe",
+            'email': "jane@14ladders.com",
+            'phone': "5553334444",
+            'date_of_birth': "1981-11-19",
+            'ssn': "456-45-4567",
+            'address': {
+                'street_address': "111 Main St",
+                'locality': "Chicago",
+                'region': "IL",
+                'postal_code': "60622"
+            }
+        },
+        'business': {
+            'legal_name': "Jane's Ladders",
+            'dba_name': "Jane's Ladders",
+            'tax_id': "98-7654321",
+            'address': {
+                'street_address': "111 Main St",
+                'locality': "Chicago",
+                'region': "IL",
+                'postal_code': "60622"
+            }
+        },
+        'funding': {
+            'destination': braintree.MerchantAccount.FundingDestination.Bank,
+            'email': "funding@blueladders.com",
+            'mobile_phone': "5555555555",
+            'account_number': "1123581321",
+            'routing_number': "071101307",
+        },
+        "tos_accepted": True,
+        "master_merchant_account_id": settings.BRAINTREE_MERCHANT_ID,
+        "id": "blue_ladders_store"
+    })
+
+
+def _test_transaction_sale():
     result = braintree.Transaction.sale({
         "amount": "1000.00",
         "credit_card": {
@@ -19,24 +62,35 @@ def test_pay():
         }
     })
     return result
-    
-        
-def pay(request):
-    payment_result = test_pay()
 
-    payment_result_details = ''
-    if payment_result.is_success:
-        payment_result_details += "\nsuccess!: " + payment_result.transaction.id
-    elif payment_result.transaction:
-        payment_result_details += "\nError processing transaction:"
-        payment_result_details += "\n  message: " + payment_result.message
-        payment_result_details += "\n  code:    " + payment_result.transaction.processor_response_code
-        payment_result_details += "\n  text:    " + payment_result.transaction.processor_response_text
+
+def _format_result_sale(result_sale):
+    result_sale_details = ''
+    if result_sale.is_success:
+        result_sale_details += "\nsuccess!: " + result_sale.transaction.id
+    elif result_sale.transaction:
+        result_sale_details += "\nError processing transaction:"
+        result_sale_details += "\n  message: " + result_sale.message
+        result_sale_details += "\n  code:    " + result_sale.transaction.processor_response_code
+        result_sale_details += "\n  text:    " + result_sale.transaction.processor_response_text
     else:
-        payment_result_details += "\nmessage: " + payment_result.message
-        for error in payment_result.errors.deep_errors:
-            payment_result_details += "\nattribute: " + error.attribute
-            payment_result_details += "\n  code: " + error.code
-            payment_result_details += "\n  message: " + error.message
+        result_sale_details += "\nmessage: " + result_sale.message
+        for error in result_sale.errors.deep_errors:
+            result_sale_details += "\nattribute: " + error.attribute
+            result_sale_details += "\n  code: " + error.code
+            result_sale_details += "\n  message: " + error.message
+    return result_sale_details
 
-    return render(request, 'payments/pay.html', {'payment_result_details': payment_result_details})
+
+def pay(request):
+    result_sale = _test_transaction_sale()
+    return render(request, 'payments/pay.html', {
+        'result': _format_result_sale(result_sale)
+    })
+
+
+def create(request):
+    result_create = _test_merchantaccount_create()
+    return render(request, 'payments/create.html', {
+        'result': result_create.message
+    })
