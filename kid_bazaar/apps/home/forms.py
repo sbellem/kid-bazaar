c3986ef2814bac5076ad49efff4c5a555f6467d9
Marcin Ossowski <marcin.ossowski@gmail.com>
2014-06-22 12:05:58 +0200

    choice for categories + label for save button on kids

--- kid_bazaar/apps/home/forms.py
+++ kid_bazaar/apps/home/forms.py
@@ -3,18 +3,27 @@ from django import forms
 from . import models
 
 
+CATEGORIES = (
+    (u'Education and Toys', u'Education and Toys'),
+    (u'Clothes and Linens', u'Clothes and Linens'),
+    (u'Feeding equipment', u'Feeding equipment'),
+    (u'Furniture', u'Furniture'),
+    (u'Others', u'Others'),
+    )
+
+
 class ItemForm(forms.ModelForm):
     class Meta:
         model = models.Item
         fields = ('name', 'pic', 'description', 'category', 'price', 'age_from_years', 'age_from', 'age_to_years', 'age_to')
     name = forms.CharField(max_length=100)
     description = forms.CharField(required=False, widget=forms.Textarea)
-    category = forms.CharField(max_length=100, required=False)
+    category = forms.ChoiceField(required=False, choices=CATEGORIES)
     price = forms.DecimalField(required=False)
     age_from_years = forms.IntegerField(min_value=0, required=False, label=u'Using from age of (years)')
     age_from = forms.IntegerField(min_value=0, required=False, label=u'Using from age of (months)')
-    age_to_years = forms.IntegerField(min_value=0, required=False, label=u'Using from age of (years)')
-    age_to = forms.IntegerField(min_value=0, required=False, label=u'Using from age of (months)')
+    age_to_years = forms.IntegerField(min_value=0, required=False, label=u'Using until age of (years)')
+    age_to = forms.IntegerField(min_value=0, required=False, label=u'Using until age of (months)')
 
 
 class KidForm(forms.ModelForm):
@@ -22,5 +31,6 @@ class KidForm(forms.ModelForm):
         model = models.Kid
         fields = ('name', 'pic', 'birthday', 'sex')
     name = forms.CharField(max_length=100, required=False)
-    birthday = forms.DateField()
+    birthday = forms.DateField(widget=forms.DateInput)
+
     sex = forms.CharField(required=False)
